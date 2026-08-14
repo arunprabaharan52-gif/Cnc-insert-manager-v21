e9128a60a491770c9a5fb3fe292ea95b5842c1db
arunprabaharan52-gif <arunprabaharan52@gmail.com>
2026-08-14 22:49:50 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,15 @@
 // Firebase will be connected in the next step.
 // For now these two apps can be installed independently.
-globalThis.FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
+
+  apgglobalThis.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAWivg-wJ7J5v6eanlI5H-3Paw-EzOuCE8",
+  authDomain: "cnc-insert-manager.firebaseapp.com",
   databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  projectId: "cnc-insert-manager",
+  storageBucket: "cnc-insert-manager.firebasestorage.app",
+  messagingSenderId: "921839032080",
+  appId: "1:921839032080:web:7b049b2e7795f86fa8f795"
 };
+
 globalThis.FIREBASE_VAPID_KEY = "";
+
